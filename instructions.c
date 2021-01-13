1e6d98994e6dc780f1d3bb3977a1b81795f030a7
Mia T <mia.mcduffie.13@gmail.com>
2021-01-13 14:58:05 -0500

    Updating Files
    All added files to make the project work.

--- instructions.c
+++ instructions.c
@@ -8,7 +8,7 @@ void add32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
     uint32_t *o2 = operands[2];
-    *d = *o1+ *o2;
+    *d = *o1 + *o2;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -16,14 +16,14 @@ void add64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
     uint64_t *o2 = operands[2];
-    *d = *o1+ *o2;
+    *d = *o1 + *o2;
 }
 
 void sub32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
     uint32_t *o2 = operands[2];
-    // TODO
+    *d = *o1 - *o2;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -31,27 +31,27 @@ void sub64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
     uint64_t *o2 = operands[2];
-    // TODO
+    *d = *o1 - *o2;
 }
 
 void neg32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
-    // TODO
+    *d = *o1 * (-1);
     *(d+1) = 0; // Clear upper 32 bits
 }
 
 void neg64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
-    // TODO
+    *d = *o1 * (-1);
 }
 
 void mul32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
     uint32_t *o2 = operands[2];
-    // TODO
+    *d = *o1 * *o2;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -59,14 +59,14 @@ void mul64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
     uint64_t *o2 = operands[2];
-    // TODO
+    *d = *o1 * *o2;
 }
 
 void udiv32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
     uint32_t *o2 = operands[2];
-    // TODO
+    *d = *o1 / *o2;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -74,14 +74,14 @@ void sdiv64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
     uint64_t *o2 = operands[2];
-    // TODO
+    *d = *o1 / *o2;
 }
 
 void lsl32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *r = operands[1];
     uint32_t *v = operands[2];
-    // TODO
+    *d = *r << *v;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -89,14 +89,14 @@ void lsl64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *r = operands[1];
     uint64_t *v = operands[2];
-    // TODO
+    *d = *r << *v;
 }
 
 void lsr32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *r = operands[1];
     uint32_t *v = operands[2];
-    // TODO
+    *d = *r >> *v;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -104,14 +104,14 @@ void lsr64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *r = operands[1];
     uint64_t *v = operands[2];
-    // TODO
+    *d = *r >> *v;
 }
 
 void and32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
     uint32_t *o2 = operands[2];
-    // TODO
+    *d = *o1 & *o2;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -119,14 +119,14 @@ void and64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
     uint64_t *o2 = operands[2];
-    // TODO
+    *d = *o1 & *o2;
 }
 
 void orr32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *o1 = operands[1];
     uint32_t *o2 = operands[2];
-    // TODO
+    *d = *o1 | *o2;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
@@ -134,53 +134,55 @@ void orr64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *o1 = operands[1];
     uint64_t *o2 = operands[2];
-    // TODO
+    *d = *o1 | *o2;
 }
 
 void ldr32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *addr = operands[1];
-    // TODO
+    *d = *addr;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
 void ldr64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *addr = operands[1];
-    // TODO
+    *d = *addr;
 }
 
 void str32(uint32_t **operands) {
     uint32_t *s = operands[0];
     uint32_t *addr = operands[1];
-    // TODO
+    *addr = *s;
 }
 
 void str64(uint64_t **operands) {
     uint64_t *s = operands[0];
     uint64_t *addr = operands[1];
-    // TODO
+    *addr = *s;
 }
 
 void mov32(uint32_t **operands) {
     uint32_t *d = operands[0];
     uint32_t *s = operands[1];
-    // TODO
+    *d = *s;
     *(d+1) = 0; // Clear upper 32 bits
 }
 
 void mov64(uint64_t **operands) {
     uint64_t *d = operands[0];
     uint64_t *s = operands[1];
-    // TODO
+    *d = *s;
 }
-
+//char *bl[] = {"bl", "400ABC", "<test>", NULL};
 void bl(uint64_t **operands) {
-    // TODO
+    machine.registers[x30] = machine.registers[pc];
+    uint64_t *d = operands[0];
+    machine.registers[pc] = *d;
 }
 
 void ret(uint64_t **operands) {
-    // TODO
+    machine.registers[pc] = machine.registers[x30];
 }
 
 struct opcode32 opcodes32[] = {
@@ -226,12 +228,14 @@ void ldp(char **instruction) {
     char **addr = parse_addr(instruction[3]); 
 
     // ldp D1, D2, [X0]
+    //--> D1 = *(X0), D2 = *(X0+8) 
+    //(loads the value at X0 and X0+8 into registers D1 and D2 respectively)
     if (addr[1] == NULL && instruction[4] == NULL) {
         printf("\tLoad-only\n");
 
         // Load the first value
         char first_addr[6];
-        sprintf(first_addr, "[%s]", addr[0]);
+        sprintf(first_addr, "[%s]", addr[0]); 
         char *first[] = { "ldr", instruction[1], first_addr, NULL };
         printf("\t");
         execute(first);
@@ -243,13 +247,66 @@ void ldp(char **instruction) {
         printf("\t");
         execute(second);
     }
+    //e.g. ldp D1, D2, [X0, #0x10]! 
+    // --> X0 = X0 + 0x10, then sets D1 = *(X0), D2 = *(X0+8)
     else if (addr[1] != NULL && instruction[4] == NULL) {
         printf("\tUpdate-then-load\n");
-        // TODO
+        printf("addr[0]:%s and addr[1]:%s\n", addr[0], addr[1]);
+        // Updates value using constant // X0 = X0 + 0x10
+        char constant[10];
+        sprintf(constant, "%s", addr[1]);
+        char *update[] = {"add", "sp", "sp", constant, NULL };
+        printf("\t");
+        execute(update);
+        print_machine();
+
+        
+        // Load the first value //D1 = *(X0)
+        char first_addr[6];
+        sprintf(first_addr, "[%s", addr[0]);
+        char *first[] = { "ldr", instruction[1], first_addr, NULL };
+        printf("\t");
+        execute(first);
+        print_machine();
+
+
+        // Load the second value //D2 = *(X0+8),
+        char second_addr[9];
+        sprintf(second_addr, "[%s,#8]", addr[0]);
+        char *second[] = { "ldr", instruction[2], second_addr, NULL };
+        printf("\t");
+        execute(second);
+
     }
+
+    //e.g. ldp D1, D2, [X0], #0x10
     else if (addr[1] == NULL && instruction[4] != NULL) {
         printf("\tLoad-then-update\n");
-        // TODO
+
+        // Load the first value //D1 = *(X0)
+        char first_addr[6];
+        sprintf(first_addr, "[%s]", addr[0]);
+        char *first[] = { "ldr", instruction[1], first_addr, NULL };
+        printf("\t");
+        execute(first);
+        print_machine();
+
+        // Load the second value //D2 = *(X0+8),
+        char second_addr[9];
+        sprintf(second_addr, "[%s,#8]", addr[0]);
+        char *second[] = { "ldr", instruction[2], second_addr, NULL };
+        printf("\t");
+        execute(second);
+        print_machine();
+
+        // Updates value using constant // X0 = X0 + 0x10
+        char constant[6];
+        sprintf(constant, "%s", instruction[4]);
+        char *update[] = {"add", "sp", "sp", constant, NULL };
+        printf("\t");
+        execute(update);
+        
+        
     }
     else {
         printf("Unhandled %s instruction\n", instruction[0]);
@@ -263,15 +320,79 @@ void stp(char **instruction) {
     // stp D1, D2, [X0]
     if (addr[1] == NULL && instruction[4] == NULL) {
         printf("\tStore-only\n");
-        // TODO
+        
+        // Store the first value //*(X0) = D1
+        char first_addr[6];
+        sprintf(first_addr, "[%s]", addr[0]);
+        char *first[] = { "str", instruction[1], first_addr, NULL };
+        printf("\t");
+        execute(first);
+
+        // Store the second value //*(X0+8) = D2
+        char second_addr[9];
+        sprintf(second_addr, "[%s,#8]", addr[0]);
+        char *second[] = { "str", instruction[2], second_addr, NULL };
+        printf("\t");
+        execute(second);
+
     }
+
+    //e.g. stp D1, D2, [X0, #-16]!
     else if (addr[1] != NULL && instruction[4] == NULL) {
         printf("\tUpdate-then-store\n");
-        // TODO
+        
+        // Update value using constant // X0 = X0 - 16
+        char constant[6];
+        sprintf(constant, "%s", addr[1]); //addr[] = {x0, #-16};
+        char *update[] = {"add", "sp", "sp", constant, NULL };
+        printf("\t");
+        execute(update);
+        print_machine();
+
+        // Store the first value //*(X0) = D1
+        char first_addr[6];
+        sprintf(first_addr, "[%s]", addr[0]);
+        char *first[] = { "str", instruction[1], first_addr, NULL };
+        printf("\t");
+        execute(first);
+        print_machine();
+
+        // Store the second value //*(X0+8) = D2
+        char second_addr[9];
+        sprintf(second_addr, "[%s,#8]", addr[0]);
+        char *second[] = { "str", instruction[2], second_addr, NULL };
+        printf("\t");
+        execute(second);
+        
+
     }
+    //e.g. stp D1, D2, [X0], #-16
     else if (addr[1] == NULL && instruction[4] != NULL) {
         printf("\tStore-then-update\n");
-        // TODO
+        
+        // Store the first value //D1 = *(X0)
+        char first_addr[6];
+        sprintf(first_addr, "[%s]", addr[0]);
+        char *first[] = { "str", instruction[1], first_addr, NULL };
+        printf("\t");
+        execute(first);
+        print_machine();
+
+        // Store the second value //D2 = *(X0+8),
+        char second_addr[9];
+        sprintf(second_addr, "[%s,#8]", addr[0]);
+        char *second[] = { "str", instruction[2], second_addr, NULL };
+        printf("\t");
+        execute(second);
+
+        // Updates value using constant // X0 = X0 -16
+        char constant[6];
+        sprintf(constant, "%s", instruction[4]);
+        char *update[] = {"add", "sp", "sp", constant, NULL };
+        printf("\t");
+        execute(update);
+        print_machine();
+        
     }
     else {
         printf("Unhandled %s instruction\n", instruction[0]);
--- parse.c
+++ parse.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdio.h>
 #include "machine.h"
 #include "parse.h"
 
@@ -8,9 +9,69 @@
  * Divides a string containing an ARM assembly instruction into an array of strings.
  */
 char **parse_instruction(char *instruction) {
-    // TODO
-    return NULL;
+    char ** test = NULL;
+    test = (char**)malloc(sizeof(char*)*20); //malloc the whole array 
+
+    for(int i=0; i<50; i++) {
+       test[i] = (char*)malloc(sizeof(char)*20); //malloc each part of the array
+    }
+    unsigned int length = strlen(instruction);
+    char test_string[50];
+    int count = 0;
+    int instruction_count = 0;
+
+    for(int x = 0; x < length; x++){
+        // TEST FOR EACH SECTION
+        //printf("%c\n", instruction[x]);
+
+        if (isspace(instruction[x])) {
+            if(test_string[count-1] == ','){
+                test_string[count-1] = '\0';
+            } else {
+                test_string[count] = '\0';
+            }
+            test_string[count] = '\0';
+            //Puts the string into the array
+            strcpy(test[instruction_count], test_string);
+
+            // TEST FOR INSTR_COUNT and ITEM IN COUNT
+            // printf("%s\n", test[instruction_count]);
+            // printf("instruction count: %d\n\n", instruction_count);
+
+            //REST STRING
+            count = 0;
+            instruction_count+=1;
+            test_string[0] = '\0';
+            
+
+        } else {
+            test_string[count] = instruction[x];
+            count++;
+        
+        }
+        
+    }
+
+    //SETTING THE ADDRESS || SECOND OPERAND
+    test_string[count] = '\0';
+    strcpy(test[instruction_count], test_string);
+
+    //SETTING THE LAST STRING TO NULL
+    //printf("The instruction count: %d\n", instruction_count);
+    test[instruction_count + 1] = NULL;
+
+    // TESTS FOR ALL PARTS
+    // printf("\nThe returned opcode: %s\n", test[0]);
+    // printf("\nThe returned destination: %s\n", test[1]);
+    // printf("\nThe first operand: %s\n", test[2]);
+    // printf("\nThe rsecond operand || parse address: %s\n", test[3]);
+    // printf("\nThe NULL: %s\n", test[4]);
+    // printf("\n");
+
+    return test;
 }
+
+
 
 /*
  * Divides a string containing an ARM memory operand into an array of strings.
@@ -35,7 +96,10 @@ char **parse_addr(char *addr) {
     }
 
     // Removing closing square bracket
-    addr[len-1] = '\0';
+    if (addr[len-1] == ']') {
+        addr[len-1] = '\0';
+    }
+    
 
     return parts;
 }
